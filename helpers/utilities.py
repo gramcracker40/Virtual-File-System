6937f98d83e35ec7d467c907728c0e8da65007e5
Kolten Pulliam <klpulliam44@gmail.com>
2023-10-11 11:29:54 -0500

    Confirm Path is done but needs polish

--- helpers/utilities.py
+++ helpers/utilities.py
@@ -44,48 +44,86 @@ def confirm_path(path:str, session_id:str) -> tuple((int, str)):
     try:
         path_type = "abs" if path[0] == "/" else "rel"
         path_parts = path.split("/")
+        path_parts = [i for i in path_parts if i != ""]
         dir_name = path_parts[-1]
+        print(dir_name)
         path_parts.reverse()
-        potential_paths = PathModel.query.filter(PathModel.file_name == dir_name)
         original_cwd_id = sessions[session_id]["cwd_id"]
         temp_cwd_id = original_cwd_id
+        path_ids = {}
+        
+        if dir_name != "..":
+            print("dir_name != '..'")
+            potential_paths = PathModel.query.filter(PathModel.file_name == dir_name)
+        
 
-        for potential_path in potential_paths:
-            initial_id = potential_path.id
+            for potential_path in potential_paths:
+                if potential_path.file_type != "directory":
+                    continue
+                initial_id = potential_path.id
+                if path_type == "rel":
+                    path_parts.reverse()
+                    for part in path_parts:
+                        # TODO: Create exception handling for TypeError when the PathModel returned from a query results in a None type
+                        # TODO: Check for when the path has no dir_name at the end, for example: ../
+                        # TODO: Fix the issue with skipping the path and going straight to an existing directory we aren't connected to
+                        if part == dir_name: # Check for NoneType before returning 
+                            candidate_path = PathModel.query.filter(PathModel.file_name == dir_name and PathModel.pid == temp_cwd_id).first()
+                            reconstructed_path = construct_path(candidate_path.id)
+                            return (candidate_path.id, reconstructed_path) if candidate_path != None else (-1, "")
+                        
+                        if part == "..":
+                            current_dir = PathModel.query.filter(PathModel.id == temp_cwd_id).first()
+                            temp_cwd_id = current_dir.pid
+                        else:
+                            child_dir = PathModel.query.filter(PathModel.file_name == part and PathModel.pid == temp_cwd_id).first()
+                            temp_cwd_id = child_dir.id
+                        
+                else: 
+                    # absolute path must end up at root, 
+                    #  so if you can trace the pid's back to 0 it exists
+                    # Save the initial file/path id
+
+                    for part in path_parts:
+                        # Check the file name, if it matches the path part, move up to the parent and get the name. Repeat until at root(id of 0)
+                        if potential_path.pid == 0:
+                            return (initial_id, path)
+                        
+                        # Decides if we move on to the next pid
+                        if potential_path.file_name == part:
+                            potential_path = PathModel.query.filter(PathModel.id == potential_path.pid).first()
+                        else:
+                            return (-1, "")
+            return (-1, "")
+        if dir_name == "..":
+            print("dir_name == '..'")
             if path_type == "rel":
-                path_parts.reverse()
                 for part in path_parts:
-                    # TODO: Create exception handling for TypeError when the PathModel returned from a query results in a None type
-                    # TODO: Check for when the path has no dir_name at the end, for example: ../
-                    if part == dir_name:
-                        candidate_path = PathModel.query.filter(PathModel.file_name == dir_name and PathModel.pid == temp_cwd_id).first()
-                        # if candidate_path == None:
-                            # raise TypeError
-                        reconstructed_path = construct_path(candidate_path.id)
-                        return (candidate_path.id, reconstructed_path) if candidate_path != None else (-1, "")
-                    
                     if part == "..":
                         current_dir = PathModel.query.filter(PathModel.id == temp_cwd_id).first()
-                        temp_cwd_id = current_dir.pid
+                        if current_dir != None:
+                            print(current_dir.id)
+                            print(current_dir.pid)
+                            # if current_dir.pid != 0:
+                            temp_cwd_id = current_dir.pid
                     else:
                         child_dir = PathModel.query.filter(PathModel.file_name == part and PathModel.pid == temp_cwd_id).first()
-                        temp_cwd_id = child_dir.id
-                    
-            else: 
-                # absolute path must end up at root, 
-                #  so if you can trace the pid's back to 0 it exists
-                # Save the initial file/path id
+                        if child_dir != None:
+                            temp_cwd_id = child_dir.id
+                if temp_cwd_id < 0:
+                    return (-1, "")
+                if temp_cwd_id == 0:
+                    reconstructed_path = construct_path(0)
+                    return (0, reconstructed_path)
+                candidate_path = PathModel.query.filter(PathModel.id == temp_cwd_id).first()
+                reconstructed_path = construct_path(candidate_path.id)
+                return (candidate_path.id, reconstructed_path) if candidate_path != None else (-1, "")
 
-                for part in path_parts:
-                    # Check the file name, if it matches the path part, move up to the parent and get the name. Repeat until at root(id of 0)
-                    if potential_path.pid == 0:
-                        return (initial_id, path)
-                    
-                    # Decides if we move on to the next pid
-                    if potential_path.file_name == part:
-                        potential_path = PathModel.query.filter(PathModel.id == potential_path.pid).first()
-                    else:
-                        return (-1, "")
+            # candidate_path = PathModel.query.filter(PathModel.id == temp_cwd_id).first()
+            # reconstructed_path = construct_path(candidate_path.id)
+            # print(candidate_path)
+            # print(reconstructed_path)
+            # return (candidate_path.id, reconstructed_path) if candidate_path != None else (-1, "")
     except TypeError as err:
         # print("Something went wrong")
         abort(404, message=f"{err}")
